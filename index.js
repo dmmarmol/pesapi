08bfa8a21898dbea55c01fc59e1684dea8e78da2
Diego Martín Mármol <diegomartinmarmol@gmail.com>
2018-06-17 13:10:39 -0300

    Add: Configuration files and attempt using webscrapper package

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require("babel-core");
 const path = require("path");
+const config = require("./src/config");
+const scrape = require("website-scraper");
+// const pckg = require("./package.json");
+// console.log(pckg);
 
-const Crawler = require("./src/Crawler/Crawler");
-
-new Crawler();
+scrape({
+	urls: [config.BASE_URL],
+	directory: "./output/v0.0.5"
+	// directory: `./output/v${pckg.version}`
+})
+	.then(result => {
+		console.log(result);
+	})
+	.catch(err => {
+		throw err;
+	});
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const DEBUG = true;
+const MAX_PAGES = 3;
+const CONCURRENCY = 1;
+const BASE_URL = "http://pesdb.net/pes2018/";
+
+module.exports = { BASE_URL, DEBUG, MAX_PAGES, CONCURRENCY };

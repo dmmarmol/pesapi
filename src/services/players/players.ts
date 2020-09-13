a151cd70af7e0576b4c837c8f83eb0000f8c81c3
unknown <diegomartinmarmol@gmail.com>
2020-09-13 20:28:02 -0300

    feat: rename vars

--- src/services/players/players.ts
+++ src/services/players/players.ts
@@ -2,7 +2,7 @@ const { getPlayerIdsFromPage } = require("../page");
 const { logger, logSeparator } = require("../../tools/logger");
 const { useState } = require("../../tools/state");
 const { chunk, minToSec } = require("../../tools/utils");
-const { sleep, countdown } = require("../../tools/request");
+const { countdown } = require("../../tools/request");
 const { REQUEST_TIMEOUT_MINUTES } = require("../../env");
 
 const REQUEST_TIMEOUT_SECONDS = minToSec(REQUEST_TIMEOUT_MINUTES);
@@ -21,7 +21,7 @@ function makeRequestIdsFromGroup() {
     failed: [],
   };
 
-  return async function requestIds(group: number[]): Promise<number[]> {
+  return async function requestIds(pageGroup: number[]): Promise<number[]> {
     logSeparator();
     logger.info(`Getting Player Ids`);
 
@@ -30,34 +30,38 @@ function makeRequestIdsFromGroup() {
      * through a for .. of loop + await-ing every request
      * the result of each request is pushed into the results array
      */
-    for (const id of group) {
+    for (const page of pageGroup) {
       const req = async (id): Promise<number[]> => {
         // Sleep 3 seconds before request
-        await sleep(3000);
+        await countdown(3);
         return await getPlayerIdsFromPage(id);
       };
       try {
-        const playerIds = await req(id).then((playerIds) => {
+        const playerIds = await req(page).then((playerIds) => {
           logger.info(
-            `Requested ids (${playerIds.length}): ${playerIds.slice(0, 5)}...`
+            `Requested ids (${playerIds.length}): ${playerIds.slice(0, 5)}${
+              playerIds.length > 5 && "..."
+            }`
           );
           return playerIds;
         });
         results.succeed.push(...playerIds);
       } catch (err) {
-        logger.info(`There was an error while fetching player id "${id}"`);
-        results.failed.push(id);
+        logger.info(`There was an error while fetching player id "${page}"`);
+        results.failed.push(page);
       }
     }
 
     if (results.failed.length) {
       logger.info(`Failed: ${results.failed.length} player Ids. Refetching`);
-      // Wait four minutes between each group
+      // Wait four minutes
       await countdown(REQUEST_TIMEOUT_SECONDS);
-      // recursively call failed ids again
+      // Recursively call failed ids again
       await requestIds(results.failed);
     }
 
+    logger.info(`All Ids found!`);
+    logger.info(`Succeed: ${results.succeed.length} player Ids`);
     return results.succeed.sort((a, b) => a - b);
   };
 }
@@ -75,7 +79,7 @@ export async function getPlayerIdsByPage(): Promise<number[]> {
   const { totalPages } = state;
   // const arrayOfPages: number[] = Array.from(Array(totalPages), (_, i) => i + 1);
   const arrayOfPages: number[] = Array.from(Array(40), (_, i) => i + 1);
-  const groupedIds: number[][] = chunk(20, arrayOfPages);
+  const groupedPages: number[][] = chunk(20, arrayOfPages);
 
   async function requestIds() {
     logSeparator();
@@ -88,13 +92,14 @@ export async function getPlayerIdsByPage(): Promise<number[]> {
      */
     let result: number[] = [];
     let count = 0;
-    for (const group of groupedIds) {
+    for (const pageGroup of groupedPages) {
       // Wait a few minutes between each group
       if (count !== 0) await countdown(REQUEST_TIMEOUT_SECONDS);
-      const ids = await requestIdsFromGroup(group);
-      result.push(...ids);
+      const playerIds = await requestIdsFromGroup(pageGroup);
+      result.push(...playerIds);
       count = count + 1;
     }
+    // sort results in an ascending way
     return result.sort((a, b) => a - b);
   }
 
